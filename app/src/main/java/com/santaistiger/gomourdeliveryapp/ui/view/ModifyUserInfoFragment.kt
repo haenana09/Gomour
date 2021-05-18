70fa239ca27ad819fa50ad0e438bdde0e50fee33
parkjieun16 <penny0123@naver.com>
2021-05-18 22:57:51 +0900

    회원 탈퇴 백스택 관리, storage 관련 파일 생성

--- app/src/main/java/com/santaistiger/gomourdeliveryapp/ui/view/ModifyUserInfoFragment.kt
+++ app/src/main/java/com/santaistiger/gomourdeliveryapp/ui/view/ModifyUserInfoFragment.kt
@@ -1,5 +1,6 @@
 package com.santaistiger.gomourdeliveryapp.ui.view
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
@@ -232,6 +233,7 @@ class ModifyUserInfoFragment: Fragment() {
     }
 
     // 탈퇴
+    @SuppressLint("RestrictedApi")
     private fun withdrawal(){
         var currentUser = Firebase.auth.currentUser
         currentUser.delete()
@@ -239,7 +241,6 @@ class ModifyUserInfoFragment: Fragment() {
                 Toast.makeText(context,"탈퇴 실패",Toast.LENGTH_LONG).show()
             }
             .addOnSuccessListener {
-                Toast.makeText(context,"탈퇴 성공",Toast.LENGTH_LONG).show()
             }
 
         db.collection("deliveryMan").document(currentUser.uid)
@@ -252,7 +253,13 @@ class ModifyUserInfoFragment: Fragment() {
                 editor.clear()
                 editor.commit()
 
-                findNavController().navigate(R.id.action_modifyUserInfoFragment_to_loginFragment)
+                // 백스택 제거
+                for (i in 1..findNavController().backStack.count()) {
+                    findNavController().popBackStack()
+                }
+
+                // 로그인 페이지로 이동
+                findNavController().navigate(R.id.loginFragment)
             }
             .addOnFailureListener { Toast.makeText(context,"탈퇴 실패", Toast.LENGTH_LONG).show() }
     }
